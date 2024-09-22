94a088732862d580bcba4015b7538f2f128fe323
Colby <conish17913@gmail.com>
2024-09-22 02:06:20 -0400

    small updates

--- include/MainController.h
+++ include/MainController.h
@@ -9,6 +9,7 @@ class MainController : public drogon::HttpController<MainController>
   public:
     METHOD_LIST_BEGIN
     ADD_METHOD_TO(MainController::DefaultRoute, "/", "InsecureFilter");
+    ADD_METHOD_TO(MainController::DefaultRoute, "/home", "InsecureFilter");
     METHOD_LIST_END
     void DefaultRoute(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) const;
 };
